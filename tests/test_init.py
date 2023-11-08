844a5f104a5f0dd5bbce6e47225643bfe3d12ba9
J. Nick Koston <nick@koston.org>
2023-11-08 09:38:07 -0600

    feat: add the fast dispatcher (#1)

---
+++ tests/test_init.py
@@ -0,0 +1,76 @@
+import asyncio
+from pathlib import Path
+from unittest.mock import MagicMock
+
+from aiohttp import web
+from yarl import URL
+
+from aiohttp_fast_url_dispatcher import FastUrlDispatcher, attach_fast_url_dispatcher
+
+
+def test_attach_fast_url_dispatcher() -> None:
+    """Test attaching the fast url dispatcher to an app."""
+    dispatcher = FastUrlDispatcher()
+    app = web.Application()
+    attach_fast_url_dispatcher(app, dispatcher)
+    assert isinstance(app._router, FastUrlDispatcher)
+    app = web.Application(router=dispatcher)
+    assert isinstance(app._router, FastUrlDispatcher)
+
+
+async def test_dispatch(tmp_path: Path) -> None:
+    """Test the FastUrlDispatcher."""
+    dispatcher = FastUrlDispatcher()
+    index_path = tmp_path.joinpath("index.html")
+    index_path.write_text("Hello World!")
+    dispatcher.add_static("/static", tmp_path, show_index=True)
+    payload = MagicMock()
+    protocol = MagicMock()
+
+    url = URL("/static/index.html")
+    message = MagicMock(url=url, method="GET")
+    request = web.Request(
+        message,
+        payload,
+        protocol=protocol,
+        host="example.com",
+        task=MagicMock(),
+        loop=asyncio.get_running_loop(),
+        payload_writer=MagicMock(),
+    )
+    assert request.rel_url == url
+    match_info = await dispatcher.resolve(request)
+    assert match_info is not None
+    assert match_info.route.resource.canonical == "/static"
+
+    url = URL("/static/index.html")
+    message = MagicMock(url=url, method="WRONG")
+    request = web.Request(
+        message,
+        payload,
+        protocol=protocol,
+        host="example.com",
+        task=MagicMock(),
+        loop=asyncio.get_running_loop(),
+        payload_writer=MagicMock(),
+    )
+    assert request.rel_url == url
+    match_info = await dispatcher.resolve(request)
+    assert match_info is not None
+    assert match_info.route.status == 405
+
+    url = URL("/not_registered")
+    message = MagicMock(url=url, method="GET")
+    request = web.Request(
+        message,
+        payload,
+        protocol=protocol,
+        host="example.com",
+        task=MagicMock(),
+        loop=asyncio.get_running_loop(),
+        payload_writer=MagicMock(),
+    )
+    assert request.rel_url == url
+    match_info = await dispatcher.resolve(request)
+    assert match_info is not None
+    assert match_info.route.status == 404
